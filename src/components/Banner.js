b77cd8dfb2b3360156a5d021628d62c7bc2d8d56
andre <andre_w3@yahoo.com>
2019-12-28 01:43:30 -0500

    changed theme and look

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import pic01 from '../assets/images/pic01.jpg'
+import { Link as ScrollLink } from 'react-scroll'
+
+const Banner = props => (
+  <section id="banner">
+    <div className="content">
+      <header>
+        <h2>C&J Pest Management</h2>
+        <p>
+          Friendly knowledgeable service.
+      
+        </p>
+      </header>
+      <span className="image">
+        <img src={pic01} alt="intro" />
+      </span>
+    </div>
+    <ScrollLink
+      to="one"
+      className="goto-next"
+      activeClass="active"
+      smooth={true}
+      offset={50}
+      duration={1500}
+      spy={true}
+    >
+      Next
+    </ScrollLink>
+  </section>
+)
+
+export default Banner
